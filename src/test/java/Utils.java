c6bf3e2e2e96aee11bd698a8db10705efb4f9a35
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-12-28 20:10:27 +0700

    Add code and configuration Utils

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,2 +1,5 @@
 public class Utils {
+
+    final static String BASE_URL = "https://formy-project.herokuapp.com/form";
+    final static String CHROME_DRIVER_LOCATION = "D:\\chromedriver\\chromedriver";
 }
